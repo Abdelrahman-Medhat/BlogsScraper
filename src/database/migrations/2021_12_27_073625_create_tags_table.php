172e512f2279d18463a38bd69347cb08bf9b9aef
Abdelrahman-Medhat <abdelrahmanmedhay@gmail.com>
2021-12-28 07:21:54 +0200

    trash

--- src/database/migrations/2021_12_27_073625_create_tags_table.php
+++ src/database/migrations/2021_12_27_073625_create_tags_table.php
@@ -14,7 +14,7 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->longText('slug');
             $table->timestamps();
