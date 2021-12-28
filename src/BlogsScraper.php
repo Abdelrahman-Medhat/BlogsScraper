123ad014e23c036f6c525de2c57225c55755c45e
Abdelrahman-Medhat <abdelrahmanmedhay@gmail.com>
2021-12-28 09:00:03 +0200

    trash

--- src/BlogsScraper.php
+++ src/BlogsScraper.php
@@ -13,7 +13,7 @@ class BlogsScraper
     public $blog; 
     public $websiteName; 
     public $tag; 
-    public $page = 0; 
+    public $pages = 0; 
     public $post_index = 0; 
 
     public  function createSlug($title){
@@ -21,87 +21,89 @@ class BlogsScraper
         return $slug;
     }
 
-    public function scrape($websiteName, $tag, $page){
+    public function scrape($websiteName, $tag, $pages){
         $this->websiteName =  $websiteName;
         $this->tag =  $tag;
-        $this->page =  $page;
+        $this->pages =  $pages;
 
         $websiteClass = $websiteName.'Parser';
         $className ='App\Parsers\\'.$websiteClass;
         $this->blog =  new $className();
         
-        $this->blog->posts(Goutte::request('GET', 
-            str_replace('{{page}}',$page,str_replace('{{tag}}',$tag,$this->blog::$websiteQuery))
-        ))->each(function ($node) {
+        foreach($pages as $page){
+            $this->blog->posts(Goutte::request('GET', 
+                str_replace('{{page}}',$page,str_replace('{{tag}}',$tag,$this->blog::$websiteQuery))
+            ))->each(function ($node) {
 
-            $post = Post::where([
-                'slug' => $this->createSlug($this->blog->postTitle($node))
-            ])->first();
+                $post = Post::where([
+                    'slug' => $this->createSlug($this->blog->postTitle($node))
+                ])->first();
 
-            if(empty($post)){
+                if(empty($post)){
 
-    
-                $category = Category::where('slug' , $this->createSlug($this->blog->postCategory($node)))->first();
-                if(empty($category)){
-                    $category = new Category;
-                    $category->name = $this->blog->postCategory($node);
-                    $category->slug = $this->createSlug($this->blog->postCategory($node));
-                    $category->save();
-                }
+                    $getPost = Goutte::request('GET', $this->blog->postLink($node));
 
-                $getPost = Goutte::request('GET', $this->blog->postLink($node));
-                
-                $blog = Blog::where('slug' , $this->createSlug($this->websiteName))->first();
-                if(empty($blog)){
-                    $blog = new Blog;
-                    $blog->name = $this->websiteName;
-                    $blog->slug = $this->createSlug($this->websiteName);
-                    $blog->domain = $this->blog::$websiteUrl;
-                    $blog->save();
-                }
-                
-                $author = Author::where('slug' , $this->createSlug($this->blog->postAuthorSelector($getPost)))->first();
-                if(empty($author)){
-                    $author = new Author;
-                    $author->name = $this->blog->postAuthorSelector($getPost);
-                    $author->slug = $this->createSlug($this->blog->postAuthorSelector($getPost));
-                    $author->save();
-                }
-
-                $tags = $this->blog->postTagsSelector($getPost);
-                $tags_ids = [];
-                foreach($tags as $nodeTag){
-                    $tag = Tag::where('slug' , $this->createSlug($nodeTag))->first();
-                    if(empty($tag)){
-                        $tag = new Tag;
-                        $tag->name = $nodeTag;
-                        $tag->slug = $this->createSlug($nodeTag);
-                        $tag->save();
+                    $category = Category::where('slug' , $this->createSlug($this->blog->postCategory($node)))->first();
+                    if(empty($category)){
+                        $category = new Category;
+                        $category->name = $this->blog->postCategory($node);
+                        $category->slug = $this->createSlug($this->blog->postCategory($node));
+                        $category->save();
                     }
-                    $tags_ids []= $tag->id;
+
+                    $blog = Blog::where('slug' , $this->createSlug($this->websiteName))->first();
+                    if(empty($blog)){
+                        $blog = new Blog;
+                        $blog->name = $this->websiteName;
+                        $blog->slug = $this->createSlug($this->websiteName);
+                        $blog->domain = $this->blog::$websiteUrl;
+                        $blog->save();
+                    }
+                    
+                    $author = Author::where('slug' , $this->createSlug($this->blog->postAuthor($getPost)))->first();
+                    if(empty($author)){
+                        $author = new Author;
+                        $author->name = $this->blog->postAuthor($getPost);
+                        $author->slug = $this->createSlug($this->blog->postAuthor($getPost));
+                        $author->save();
+                    }
+
+                    $tags = $this->blog->postTags($getPost);
+                    $tags_ids = [];
+                    foreach($tags as $nodeTag){
+                        $tag = Tag::where('slug' , $this->createSlug($nodeTag))->first();
+                        if(empty($tag)){
+                            $tag = new Tag;
+                            $tag->name = $nodeTag;
+                            $tag->slug = $this->createSlug($nodeTag);
+                            $tag->save();
+                        }
+                        $tags_ids []= $tag->id;
+                    }
+                    
+                    $post = Post::create([
+                        'author_id' => $author->id,
+                        'blog_id' => $blog->id,
+                        'category_id' => $category->id,
+                        'title' => $this->blog->postTitle($node),
+                        'slug' => $this->createSlug($this->blog->postTitle($node)),
+                        'excerpt' => $this->blog->postExcerpt($node),
+                        'content' => $this->blog->postContent($getPost),
+                        'image' => $this->blog->postImage($node),
+                        'url' => $this->blog->postLink($node)
+                    ]);
+                    
+                    $post = Post::findOrFail($post->id);
+                    $post->tags()->attach($tags_ids);
                 }
-                
-                $post = Post::create([
-                    'author_id' => $author->id,
-                    'blog_id' => $blog->id,
-                    'category_id' => $category->id,
-                    'title' => $this->blog->postTitle($node),
-                    'slug' => $this->createSlug($this->blog->postTitle($node)),
-                    'excerpt' => $this->blog->postExcerpt($node),
-                    'content' => $this->blog->postContentSelector($getPost),
-                    'image' => $this->blog->postImage($node),
-                    'url' => $this->blog->postLink($node)
-                ]);
-                
-                $post = Post::findOrFail($post->id);
-                $post->tags()->attach($tags_ids);
-            }
-            $this->post_index++;
-        });
+                $this->post_index++;
+            });
+
+        }
     }
 
-    public function sync($websiteName){
-        $this->scrape($websiteName, 'biology', 2);
+    public function sync($websiteName,$tag, $pages){
+        $this->scrape($websiteName, $tag, $pages);
     }
 
 }
--- src/Parsers/scitechdailyParser.php
+++ src/Parsers/scitechdailyParser.php
@@ -10,29 +10,18 @@ class scitechdailyParser
 
     public static $websiteLogo = 'https://scitechdaily.com/images/cropped-scitechdaily-amp60.png';
 
-    public static $websiteCategories = [
-        'biology',
-        // 'chemistry',
-        // 'earth',
-        // 'health',
-        // 'physics',
-        // 'science',
-        // 'space',
-        // 'technology'
-    ];
-
     public static $websiteQuery = 'https://scitechdaily.com/news/{{tag}}/amp/page/{{page}}/';
 
     public function posts($postsContainer = null){
         return $postsContainer->filter('.listing-item');
     }
 
-    public function postTitle($node){
-        return $node->filter('.post-title a')->text();
-    }
-    
     public function postLink($node){
         return $node->filter('.post-title a')->attr('href');
+    }
+
+    public function postTitle($node){
+        return $node->filter('.post-title a')->text();
     }
     
     public function postImage($node){
@@ -47,22 +36,22 @@ class scitechdailyParser
         return $node->filter('.post-excerpt p')->text();
     }
 
-    public function postAuthorSelector($post){
+    public function postAuthor($post){
         return $post->filter('.post-author')->text();
     }
 
-    public function postContentSelector($post){
+    public function postContent($post){
         return str_replace('amp-img', 'img', $post->filter('.post-content')->html());
     }
 
-    public function postTagsSelector($post){
-        $postTags = [];
+    public function postTags($post){
+        $GLOBALS['postTags'] = [];
 
-        $post->filter('.tags a')->each(function ($tagsNode) {
-            $postTags []= $tagsNode->text() ;
+        $post->filter('.tags a')->each(function ($tagsNodes) {
+            $GLOBALS['postTags'] []= $tagsNodes ;
         });
 
-        return $postTags;
+        return $GLOBALS['postTags'];
     }
     
 }
