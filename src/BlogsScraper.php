ef46765e3a5253e108cefb6d7da128c671e35f99
Abdelrahman-Medhat <abdelrahmanmedhay@gmail.com>
2021-12-29 19:50:48 +0200

    dev commit

--- src/BlogsScraper.php
+++ src/BlogsScraper.php
@@ -15,7 +15,6 @@ class BlogsScraper
     public $tag; 
     public $pages = 0; 
     public $post_index = 0; 
-
     public  function createSlug($title){
         $slug=preg_replace('/[^A-Za-z0-9-]+/', '-', $title);
         return $slug;
